d4341d274afb71e89086400550d584a0cf0a4dd8
Felipe Laurindo <felipe_nu@hotmail.com>
2024-01-10 14:11:18 -0400

    Create py

---
+++ stocks-dashboard.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Jan 10 2024
+@author: Felipe Laurindo
+"""
+import pandas as pd
+
+planilha_carteira = pd.read_excel("stocks_spreadsheet.xlsx", "Carteira")
+planilha_dividendos = pd.read_excel("stocks_spreadsheet.xlsx", "Dividendos")
